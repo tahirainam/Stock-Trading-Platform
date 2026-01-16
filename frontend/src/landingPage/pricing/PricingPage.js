ef0853141a02638b725ca33b40a2bcb3eead4df5
tahirainam <cattblackwhite@gmail.com>
2026-01-16 05:34:06 -0800

    done with pricing page

--- frontend/src/landingPage/pricing/PricingPage.js
+++ frontend/src/landingPage/pricing/PricingPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Navbar from '../Navbar';
 import Hero from './Hero';
+import OpenAccount from '../OpenAccount';
 import Brokerage from './Brokerage';
-import Footer from '../Footer';
 
 function PricingPage() {
     return ( 
         <>
           <Hero/>
+          <OpenAccount/>
           <Brokerage/>
         </>
      );
