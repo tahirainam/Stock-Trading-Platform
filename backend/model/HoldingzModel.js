2efd8790a539996221ea7a250b1db1a5855c806d
tahirainam <cattblackwhite@gmail.com>
2026-04-02 11:24:48 -0700

    removed all

--- backend/model/HoldingzModel.js
+++
@@ -1,6 +0,0 @@
-const {model} = require("mongoose");
-const {HoldingzSchema} = require("../schemas/HoldingzSchema");
-
-const HoldingzModel = model("holding", HoldingzSchema);
-
-module.exports = {HoldingzModel};
--- backend/model/PositionzModel.js
+++
@@ -1,6 +0,0 @@
-const {model} = require("mongoose");
-const {PositionzSchema} = require("../schemas/PositionzSchema");
-
-const PositionzModel = model("position", PositionzSchema);
-
-module.exports = {PositionzModel};
